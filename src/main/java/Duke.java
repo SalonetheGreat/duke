b866bfd9b2a58094efb33cde422cdafd03303b8b
Sha Long <shalong5794@gmail.com>
2019-08-31 23:30:00 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import General.Message;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Message.getHello());
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            System.out.println(line);
+            if (line.equals("bye")) {
+                System.out.println(Message.getBye());
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/General/Message.java
@@ -0,0 +1,14 @@
+package General;
+
+public class Message {
+    private static String hello = "Hello! I'm Duke\nWhat can I do for you?";
+    private static String bye = "Bye! Hope to see you again soon!";
+
+    public static String getHello() {
+        return hello;
+    }
+
+    public static String getBye() {
+        return bye;
+    }
+}
